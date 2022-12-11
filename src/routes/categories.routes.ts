244435e8874524eb4db38cbdbecedc440c5b9c88
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-12-11 12:49:24 -0300

    feat: category repository and model

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,28 @@
 import { Router } from "express";
 
+import { CategoriesRepositories } from "../repositories/CategoriesRepositories";
+
 const categoriesRoutes = Router();
+const categoriesRepositories = new CategoriesRepositories();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
-    name,
-    description,
-  });
+  const categoryAlreadyExists = categoriesRepositories.findyByName(name);
 
-  response.status(201).send();
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ message: "Category Already Exists" });
+  }
+
+  categoriesRepositories.create({ name, description });
+
+  return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const allCategories = categoriesRepositories.list();
+
+  return response.json(allCategories);
 });
 
 export { categoriesRoutes };
